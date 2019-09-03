0ae7efdc55a0a797a1b92076d34163d05c32aecc
masonkarsevar <masonkarsevar@gmail.com>
2019-09-03 09:59:11 -0700

    get request sanity coded

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
 // remove a movie 
 // see only released movies 
 
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.status(200).json({api: '...api'})
+});
+
+const port = 6000;
+
+server.listen(port, () => {
+    console.log(`api on port ${port}`)
+});
