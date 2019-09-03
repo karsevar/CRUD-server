fa92503e7fba51e5ff822938b4e608d5132c6fec
masonkarsevar <masonkarsevar@gmail.com>
2019-09-03 09:52:23 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// see a list of pre-populated movies
+// add a movie to the list 
+// change or update movie information 
+// remove a movie 
+// see only released movies 
+
---
+++ notes.md
@@ -0,0 +1,8 @@
+steps 
+
+- git status 
+- mkdir foldername
+- cd folderName 
+- git init
+- npm init -y 
+- code .
